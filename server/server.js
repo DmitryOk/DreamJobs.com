deb38b208d28e36198059057fb9758d720cd5841
DmitryOK <baatezy@ya.ru>
2019-11-25 02:54:37 +0400

    Made build directory statical.

--- server/server.js
+++ server/server.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const server = express();
-const port = 3333;
+const path = require('path');
+const port = 3000;
 
-server.get('./',(request,response) => {
-    return response.send(`Server still working!`);
+server.use(express.static("build"));
+
+  server.get('/build',(request,response) => { 
+    
+    response.sendFile(path.resolve(__dirname,`build/index.html`));
+
 });
+
+server.get('/build/bundle.js',(request,response) => { 
+    
+    response.sendFile(path.resolve(__dirname,`build/bundle.js`));
+
+}); 
+
+
+
 
 server.listen(port,()=> console.log(`Server started at port ${port}`));
 
