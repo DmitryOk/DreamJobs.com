d6ed7a94d04da367be354d9b63caf5ac68a9adc7
DmitryOK <baatezy@ya.ru>
2019-11-27 00:53:35 +0400

    Now environment  tools works as planned.

---
+++ index.js
@@ -0,0 +1,6 @@
+import App from './src/app';
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+ReactDOM.hydrate(<App/>,document.getElementById('root'));
+
--- src/app.js
+++ src/app.js
@@ -1,2 +1,7 @@
 import React from 'react';
 import Redux from 'redux';
+
+ const App = () =>  <div>HELLO</div> ;
+    
+
+export default App;
