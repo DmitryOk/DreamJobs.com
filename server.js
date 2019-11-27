c3951eb91b8ab5cb93fc678382e25354a10e93c5
DmitryOK <baatezy@ya.ru>
2019-11-28 03:34:07 +0400

    Added coonection to cloud MongoDB cluster.

--- server.js
+++ server.js
@@ -2,11 +2,28 @@ const express = require('express');
 const server = express();
 const path = require('path');
 const port = 3333;
-
-
 const webpack  = require('webpack');
 const webpackConfig = require(process.env.WEBPACK_CONFIG ? process.env.WEBPACK_CONFIG : './webpack.config.dev');
 const compiler = webpack(webpackConfig);
+
+/*filer required to mongo connection*/ 
+const MongoClient = require('mongodb').MongoClient;
+const MONGO_URI_DATA = require('./const/const');
+
+const uri = `mongodb+srv://${MONGO_URI_DATA.USERNAME}:${MONGO_URI_DATA.PASSWORD}@${MONGO_URI_DATA.CLUSTER_NAME}/test?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true , useUnifiedTopology:true });
+
+client.connect((err,result) => {
+  const collection = client.db("DreamJob").collection("vacancies");
+  collection.countDocuments({
+      company:'someCompany',
+      adress:'some adress',
+      salary:'4444'
+  }).then(
+    console.log(result))
+    .then(client.close())
+    .catch(new Error(err))  
+});
 
 
 server.use(require('webpack-dev-middleware')(compiler , {
