451c2de4f5f3f4cc2170116bf288afb4eabd813a
DmitryOK <baatezy@ya.ru>
2019-12-07 01:50:17 +0400

    store displaced in own separate file

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ import App from './src/app';
 import {hydrate} from 'react-dom';
 import React from 'react';
 import {Provider} from 'react-redux';
-import {createStore} from 'redux';
+import store from './src/store/store'
 
-const store = createStore();
 
 hydrate(
 
