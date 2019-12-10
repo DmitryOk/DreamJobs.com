c239151216bfdaf5953f767578337613105eefa8
DmitryOK <baatezy@ya.ru>
2019-12-11 03:30:28 +0400

    Pass app through redux connect (attemept to increase visual store observing)

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 import App from './src/app';
 import {hydrate} from 'react-dom';
 import React from 'react';
-import {Provider} from 'react-redux';
+import {Provider,connect} from 'react-redux';
 import store from './src/store/store'
+
+const AppConnect = connect((state) => {
+   console.log(state);
+   return state;
+})(App);
 
 
 hydrate(
 
    <Provider store={store}>
-        <App/>   
+        <AppConnect/>   
    </Provider> 
     
 ,document.getElementById('root'));
