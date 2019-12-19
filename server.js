d5cc6a789d83189c3fcb864c4e164e2bea7476ed
DmitryOK <baatezy@ya.ru>
2019-12-19 17:54:57 +0400

    test query into mongo now ends succesfully

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const port = 3333;
 const webpack  = require('webpack');
 const webpackConfig = require(process.env.WEBPACK_CONFIG ? process.env.WEBPACK_CONFIG : './webpack.config.dev');
 const compiler = webpack(webpackConfig);
+const ROOT = require('./const/const');
 
 /*files required to mongo connection*/ 
 const MongoClient = require('mongodb').MongoClient;
@@ -12,18 +13,6 @@ const MONGO_URI_DATA = require('./const/const');
 
 const uri = `mongodb+srv://${MONGO_URI_DATA.USERNAME}:${MONGO_URI_DATA.PASSWORD}@${MONGO_URI_DATA.CLUSTER_NAME}/test?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true , useUnifiedTopology:true });
-
-client.connect((err,result) => {
-  const collection = client.db("DreamJob").collection("vacancies");
-  collection.countDocuments({
-      company:'someCompany',
-      adress:'some adress',
-      salary:'4444'
-  }).then(
-    console.log(result))
-    .then(client.close())
-    .catch(new Error(err))  
-});
 
 
 server.use(require('webpack-dev-middleware')(compiler , {
@@ -37,9 +26,27 @@ server.use(require('webpack-hot-middleware')(compiler , {
 
 server.use(express.static("build"));
 
-server.get('*',(request,response) => {  
+server.get('/',(request,response) => {  
     response.sendFile(path.resolve(__dirname,`build/index.html`));
 });
+
+server.get(`/vc`,(req,res)=>{
+
+    (async () => {
+        
+        try{
+            client.connect();
+            const docs = await client.db('DreamJob').collection('vacancies').find({Company_name:"567"}).toArray()
+             res.send(docs)
+             client.close()             
+        }
+        catch(err){
+            throw new Error(err);
+        }
+
+    })()
+    
+})
 
 
 server.listen(port,()=> console.log(`Server started at port ${port}`));
