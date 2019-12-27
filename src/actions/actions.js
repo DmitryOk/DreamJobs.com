6296a0e8e75925602d444958725e7f5ebef78e8e
DmitryOK <baatezy@ya.ru>
2019-12-27 20:49:23 +0400

    actions for async data fetching initiated

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,9 +1,41 @@
-import * as types from '../constants/actionTypes';
 
-export const getDailyVacancies = () => {
+export const FETCH_VACANCIES_REQUEST = 'FETCH_VACANCIES_REQUEST';
+export const FETCH_VACANCIES_SUCCESS = 'FETCH_VACANCIES_SUCCESS';
+export const FETCH_VACANCIES_REFUSE  = 'FETCH_VACANCIES_REFUSE';
+
+
+export const fetchVacanciesRequest = url => {
     return {
-        type :types.GET_DAILY_VACANCIES,
-        url:`http://localhost:3333/${types.GET_DAILY_VACANCIES.toLowerCase()}`
+        type:FETCH_VACANCIES_REQUEST,
+        status:'Pending',
+        url
     }
 }
+
+export const fetchVacanciesSuccess = response => {
+    return {
+        type:FETCH_VACANCIES_SUCCESS,
+        status:200,
+        response 
+    }
+}
+
+export const fetchVacanciesRefuse = err =>{
+    return {
+        type : FETCH_VACANCIES_REFUSE,
+        status:'FAILED',
+        err
+    }
+}
+
+const fetchVacancies = url => dispatch => {
+    dispatch(fetchVacanciesRequest(url));
+    return fetch(url)
+            .then(data => data.json())
+            .then( response => dispatch(fetchVacanciesSuccess(response)))
+            .catch(err => dispatch(fetchVacanciesRefuse(err)))
+}
+
+export default fetchVacancies;
+
 
