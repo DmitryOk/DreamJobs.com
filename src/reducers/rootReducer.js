cfc19041aaaf462db7958567e4495ff545213489
DmitryOK <baatezy@ya.ru>
2019-12-27 20:50:56 +0400

    root reducer initiated

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,25 +1,39 @@
-import {getDailyVacancies} from '../actions/actions';
+import {FETCH_VACANCIES_REFUSE,FETCH_VACANCIES_REQUEST,FETCH_VACANCIES_SUCCESS} from '../actions/actions'
 
 
 const initialState = {
-    url:'',
-    loading:false,
-    error:false
+    isFetching:false,
+    data:[]
 }
 
 const rootReducer = (state = initialState, action) => {
+
     switch(action.type){
 
-        case 'GET_DAILY_VACANCIES':
-            return (dispatch) => {
-                dispatch(getDailyVacancies);
-                fetch(action.url)
-                    .then(res => res.json())
-                    .then((data,err)=> dispatch(data))
-            };
+        case FETCH_VACANCIES_REQUEST :
+            return {
+                ...state,
+                isFetching:true,
+  
+            }
+        case FETCH_VACANCIES_SUCCESS :
+            return {
+                ...state,
+                isFetching:false,
+                data:action.response
+            }
+        case FETCH_VACANCIES_REFUSE :
+            return {
+                ...state,
+                isFetching:false,
+            }
 
-        default: return state ;
+         default :
+            return {
+                ...state
+            }   
     }
+
 }
 
 export default rootReducer;
