81fdacf632b8ac546dba14977ee854c15b7e7a5a
DmitryOK <baatezy@ya.ru>
2020-01-05 00:54:39 +0400

    Modified app file.

--- src/app.js
+++ src/app.js
@@ -1,11 +1,49 @@
-import React from 'react';
-import Header from '../src/components/containers/header';
-import Main from './components/pages/main';
+import React, { useReducer, useEffect, Component } from 'react';
+import Footer from './components/containers/footer';
+import Navigation from './components/containers/navigation';
+import { ROOT_PATH } from '../const/const';
+import fetchVacancies from './actions/actions';
+import { connect } from 'react-redux';
+import Vacancies from './components/misc/vacancyPost'
 
- const App = () => 
- 
-     <Header/>
- 
-    
 
-export default App;
+
+const url = `${ROOT_PATH}vc`;
+
+
+
+class App extends Component {
+
+    componentDidMount(){
+        const {dispatch} = this.props;
+        this.props.dispatch(fetchVacancies(url));
+    }
+
+
+    handler = event => {
+        event.preventDefault();
+        const { dispatch } = this.props;
+        dispatch(fetchVacancies(url));
+    }
+
+    render() {
+       const { data , isFetching } = this.props;
+
+        return (
+            <div>
+                <Navigation />
+                {!isFetching  ?
+                    <Vacancies posts={data}/>
+                  :  <h1>Loading...</h1>  }
+                <button onClick={this.handler}>GET VAC</button>
+                <Footer />
+            </div>
+        )
+
+    }
+}
+
+const mapStateToProps = state => ({data:state.data})
+   
+
+export default connect(mapStateToProps)(App); 
